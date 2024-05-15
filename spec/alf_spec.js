f0c2a5f6fac35a34d29e4caf4afc9e39ab54420b
Simon Marek <if23b111@technikum-wien.at>
2024-05-15 04:00:42 +0200

    X adjusted and small code change for y coordinate

--- spec/alf_spec.js
+++ spec/alf_spec.js
@@ -6,4 +6,7 @@ describe('Help Alf', function(){
 	it('should return [0,0] for given map', function(){
 		expect(spaceship.add("..........\n..........\n..........\n..........\n..........\n..........")).toEqual([0,0]);
 	});
+	it('should return [3,1] for given map', function(){
+		expect(spaceship.add("..........\n..........\n..........\n..........\n...X.......\n..........")).toEqual([3,1]);
+	});
 });
--- src/alf.js
+++ src/alf.js
@@ -5,7 +5,7 @@ SearchSpaceship.prototype.add = function(map){
 	for (let y = 0; y < lines.length; y++) {
         for (let x = 0; x < lines[y].length; x++) {
             if (lines[y][x] === 'X') {
-                let adjustedY = y - 5;
+                let adjustedY = 5 - y;
                 return [x, adjustedY];
             }
         }
