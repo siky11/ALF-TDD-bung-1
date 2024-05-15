23787b376160f067e99e7ce0701b12540f8dc5f8
Simon Marek <if23b111@technikum-wien.at>
2024-05-15 03:56:48 +0200

    first search code implementation

---
+++ src/alf.js
@@ -0,0 +1,18 @@
+function SearchSpaceship(){}
+SearchSpaceship.prototype.add = function(map){
+	let lines = map.split("\n");
+
+	for (let y = 0; y < lines.length; y++) {
+        for (let x = 0; x < lines[y].length; x++) {
+            if (lines[y][x] === 'X') {
+                let adjustedY = y - 5;
+                return [x, adjustedY];
+            }
+        }
+    }
+	if (lines.length >= 5) {
+        return [0, 0];
+    } else {
+        return "Spaceship lost forever.";
+    }
+};
